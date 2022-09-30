65c012ec646554dacfadb00c54d60f7b4d0ef93a
DoubleSpicy <stven3au@gmail.com>
2022-09-30 11:08:53 +0800

    implement custom colors

--- model.py
+++ model.py
@@ -1,11 +1,27 @@
+import sys, os
+sys.path.append(os.getcwd())
+
+# from image_processor import CustomColorChange
+
+
 import argparse
-import enum
+# import enum
+from typing import OrderedDict
+import numpy as np
+# import math
+# import random
+
+'''
+this is the image processor on the dataset so that each clinet gets MNIST images of different colors
+'''
+
+# import enum
 import os
 from typing import OrderedDict
 import numpy as np
-import math
-import sys
-import random
+# import math
+# import sys
+# import random
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
@@ -18,46 +34,103 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
+import shutil
 
-import asyncio
+import matplotlib.pyplot as plt
+os.makedirs("../../data/mnist", exist_ok=True)
 
+dir = './dataset_transform'
+if os.path.exists(dir):
+    shutil.rmtree(dir)
+os.makedirs(dir)
+
+import cv2 as cv
+from PIL import Image
+
+def BGR2PIL(img):
+    # BGR opencv -> PIL
+    cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    im_pil = Image.fromarray(img)
+    return im_pil
+
+
+class CustomColorChange:
+    ''' Change background color
+        https://pytorch.org/vision/stable/transforms.html'''
+    def __init__(self, debug=False):
+        self.debug = debug
+        return
+    
+    def __call__(self, imgPIL):
+        imgRGB = np.array(imgPIL)
+        imgBGR = cv.cvtColor(imgRGB, cv.COLOR_RGB2BGR)
+        # imgRGB = cv.cvtColor(imgRGB, cv.COLOR_BGR2BGR)
+        imgHSV = cv.cvtColor(imgBGR, cv.COLOR_RGB2HSV)
+        if self.debug:
+            cv.imwrite('testimgHSV.jpg',cv.cvtColor(imgHSV, cv.COLOR_HSV2BGR))
+
+        lower = np.array([0, 0, 0])
+        upper = np.array([180, 255, 125])
+        mask = cv.inRange(imgHSV, lower, upper)
+        cv.imwrite('testimgMask.jpg', cv.cvtColor(mask, cv.COLOR_GRAY2BGR))
+
+        color_mask = np.zeros((28, 28, 3), np.uint8)
+        color_mask[:] = (125, 180, 255) # red
+        if self.debug:
+            cv.imwrite('testimgColorMask.jpg', cv.cvtColor(color_mask, cv.COLOR_HSV2BGR))
+        color_mask = cv.bitwise_and(color_mask, color_mask, mask=mask)
+        imgBGR = cv.add(imgBGR, cv.cvtColor(color_mask, cv.COLOR_HSV2BGR))
+        if self.debug:
+            cv.imwrite('testimgMasked.jpg', cv.cvtColor(imgBGR, cv.COLOR_BGR2RGB))
+        pil_img = BGR2PIL(imgBGR)
+        if self.debug:
+            pil_img.save('testimgPIL.jpg')
+        return pil_img
+
+
+# dataset = datasets.MNIST(
+#         "../../data/mnist",
+#         train=True,
+#         download=True,
+#         transform=transforms.Compose([transforms.Grayscale(3), CustomColorChange(), transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+#     )
+# # transforms.ToTensor(),  transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+# dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True, num_workers=2)
+# i = 0
+
+# def save_image(numpy_array, name):
+#     # save this numpy_array image as jpg
+#     numpy_array = numpy_array.astype(np.uint8)
+#     img = Image.fromarray(numpy_array)
+#     img.save(name + ".jpg")
+
+def nparray_to_PIL(nparr):
+    # convert for opencv use
+    nparr *= 255
+    return Image.fromarray(nparr.astype(np.uint8))
 
 
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-n_epochs = 200
-batch_size = 64
+n_epochs = 100
+batch_size = 8
 lr = 0.00005
 n_cpu = 8
 latent_dim = 100
 img_size = 28
-channels = 1
+channels = 3
 n_critic = 5
 clip_value = 0.01
 sample_interval = 400
-client_cnt = 2
-share_D = False
-
-
-# Configure data loader
-os.makedirs("../../data/mnist", exist_ok=True)
-# dataloader = torch.utils.data.DataLoader(
-#     datasets.MNIST(
-#         "../../data/mnist",
-#         train=True,
-#         download=True,
-#         transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]),
-#     ),
-#     batch_size=batch_size,
-#     shuffle=True,
-# )
+client_cnt = 8
+share_D = True
 
 dataset = datasets.MNIST(
         "../../data/mnist",
         train=True,
         download=True,
-        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]),
+        transform=transforms.Compose([transforms.Grayscale(3), transforms.ToTensor()]),
     )
 print(len(dataset))
 # select digit = 1 and 9 only
@@ -71,17 +144,41 @@ test_len = int(len(dataset)*0.8)
 trainset, testset = random_split(dataset, [test_len, len(dataset)-test_len], generator=torch.Generator().manual_seed(42))
 print("train and test set size: ", len(trainset), len(testset))
 
+# split n sets for the train and test set
+def cal_split_lengths(length, n):
+    # divide a length into n approx. equal parts
+    div = length // n
+    ans = [div for i in range(n)]
+    ans[-1] += length - div*n
+    print(ans)
+    return ans
 
+def visualize_length(set):
+    print("length of set: ", end='')
+    print(set)
+    for i in enumerate(set):
+        print(len(i), end=' ')
+    print('')
 
-# prob = 0.1
+trainset = random_split(trainset, cal_split_lengths(len(trainset), client_cnt), generator=torch.Generator().manual_seed(42))
+testset = random_split(testset, cal_split_lengths(len(testset), client_cnt), generator=torch.Generator().manual_seed(42))
+visualize_length(trainset)
+visualize_length(testset)
 
-# prob_for_classes = [prob if val == 9 else 1-prob for val in trainset.targets] # binary prob assignment
-# prob_1 = list(random.choices(trainset.targets, weights=prob_for_classes, k=1)[0] for i in range(len(trainset)))
+# apply color filter on each trainset
+for i in range(len(trainset)):
+    trainset[i].dataset.transform = transforms.Compose([
+        transforms.ToPILImage(),
+        # transforms.Grayscale(3),
+        CustomColorChange(), 
+        # transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
+    ])
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
+trainloader = [torch.utils.data.DataLoader(trainset[i], batch_size=batch_size,
+                                            shuffle=True, num_workers=2) for i in range(client_cnt)]
+testloader = [torch.utils.data.DataLoader(testset[i], batch_size=batch_size,
+                                            shuffle=True, num_workers=2) for i in range(client_cnt)]
 
 img_shape = (channels, img_size, img_size)
 
@@ -141,7 +238,7 @@ class Server():
 
     def average(self):
         # do we need to accelerate this for large model/client counts with extern C++ calls?
-        print("averaging models on %d clients..."%(len(self.clients))) 
+        print("averaging models on %d clients..."%(len(self.clients)))
         averaged_gen = OrderedDict()
         averaged_dis = OrderedDict()
         for it, client in enumerate(self.clients):
@@ -161,10 +258,10 @@ class Server():
         print("updating models for all clients")
         for idx, client in enumerate(self.clients):
             # client.generator.model = nn.Linear(1,1)
-            client.generator.load_state_dict(gen_params)
+            self.clients[idx].generator.load_state_dict(gen_params)
             # client.discriminator.model = nn.Linear(1,1)
             if share_D:
-                client.discriminator.load_state_dict(dis_params)
+                self.clients[idx].discriminator.load_state_dict(dis_params)
         print("done.")
 
     async def _async_train(self, epoch):
@@ -172,14 +269,19 @@ class Server():
         await asyncio.wait(tasks)
         # for idx, client in enumerate(self.clients):
         #     client.train_1_epoch(epoch+1)
+    def _train(self, epoch):
+        for idx, client in enumerate(self.clients):
+            client.train_1_epoch(epoch+1)
 
     def train(self):
         for epoch in range(n_epochs):
-            asyncio.run(self._async_train(epoch))
-        self.average()
+            # asyncio.run(self._async_train(epoch))
+            self._train(epoch)
+            self.average()
 
     def val(self):
-        pass
+        server.clients[0].val()
+            
         
 class Client():
     # each client has one generator and discriminator
@@ -201,9 +303,9 @@ class Client():
 
         
 
-    async def train_1_epoch(self, epoch):
+    def train_1_epoch(self, epoch):
         # attrs = vars(self)
-        for i, (imgs, _) in enumerate(trainloader):
+        for i, (imgs, _) in enumerate(trainloader[self.id]):
 
             # Configure input
             
@@ -250,16 +352,34 @@ class Client():
 
                 print(
                     "[cid: %d] [Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                    % (self.id, epoch, n_epochs, self.batches_done % len(trainloader), len(trainloader), loss_D.item(), loss_G.item())
+                    % (self.id, epoch, n_epochs, self.batches_done % len(trainloader[self.id]), len(trainloader[self.id]), loss_D.item(), loss_G.item())
                 )
 
             if self.batches_done % sample_interval == 0:
-                save_image(gen_imgs.data[:25], "images/%d.png" % self.batches_done, nrow=5, normalize=True)
+                save_image(gen_imgs.data[:25], "images/pid%d_batch%d.png" % (self.id, self.batches_done), nrow=5, normalize=True)
             
 
             self.batches_done += 1
+
+    def val(self):
+        loss_list = list()
+        for i, (imgs, _) in enumerate(testloader[self.id]):
+            # validate the generator
+            self.optimizer_G.zero_grad()
+
+            # Sample noise as generator input
+            z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], latent_dim))))
+            # Generate a batch of images
+            gen_imgs = self.generator(z)
+            # Adversarial loss
+            loss_G = -torch.mean(self.discriminator(gen_imgs))
+            loss_list.append(loss_G.item())
+        print("validation:", "[G loss: %f]"%(sum(loss_list)/len(loss_list)))
+        
+        
+
 # make N clients
-client_list = [Client(x) for x in range(1, client_cnt+1)]
+client_list = [Client(x) for x in range(client_cnt)]
 
 # create server
 server = Server(client_list)
@@ -267,3 +387,4 @@ server = Server(client_list)
 
 # start training
 server.train()
+server.val()
--- test.py
+++ test.py
@@ -17,7 +17,9 @@
 # asyncio.run(async_loop(10))
 # print('done')
 
-# list1 = [1,2,3,4,5,6,7,8,9,10]
+list1 = [1,2,3,4,5,6,7,8,9,10]
+for idx, item in enumerate(list1):
+    print(id(list1[idx]), id(item))
 # def total_and_item(sequence):
 #     total = 0
 #     for i in sequence:
@@ -39,47 +41,47 @@
 # for i in range(10):
 #     print(random.choices(list1, weights=prob, k=1)[0])
 
-import argparse
-import enum
-import os
-from typing import OrderedDict
-import numpy as np
-import math
-import sys
-import random
+# import argparse
+# import enum
+# import os
+# from typing import OrderedDict
+# import numpy as np
+# import math
+# import sys
+# import random
 
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
+# import torchvision.transforms as transforms
+# from torchvision.utils import save_image
 
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torch.autograd import Variable
+# from torch.utils.data import DataLoader
+# from torchvision import datasets
+# from torch.autograd import Variable
 
-import torch.nn as nn
-import torch.nn.functional as F
-import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torch
 
-import asyncio
+# import asyncio
 
-trainset = datasets.MNIST(
-        "../../data/mnist",
-        train=True,
-        download=True,
-        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]),
-    )
+# trainset = datasets.MNIST(
+#         "../../data/mnist",
+#         train=True,
+#         download=True,
+#         transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]),
+#     )
 
-idx = (trainset.targets==1 and random.) | (trainset.targets==9) # select digit = 1 and 9 only
-trainset.targets = trainset.targets[idx]
-trainset.data = trainset.data[idx]
+# idx = (trainset.targets==1 and random.) | (trainset.targets==9) # select digit = 1 and 9 only
+# trainset.targets = trainset.targets[idx]
+# trainset.data = trainset.data[idx]
 
-prob = 0.1
-def rand_bool():
-    
+# prob = 0.1
+# def rand_bool():
+
 # print(trainset.classes)
 # prob_for_classes = [prob if val == 9 else 1-prob for val in trainset.targets]
 # print(prob_for_classes)
 
-print(trainset.targets)
+# print(trainset.targets)
 
 
 # def select(trainset):
